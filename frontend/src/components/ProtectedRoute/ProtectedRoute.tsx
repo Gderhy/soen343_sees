6a1198d42c4fbd8451cda886c050d725a57ca096
Gderhy <gabrielderhy@gmail.com>
2025-03-20 18:28:29 -0400

    Enhance ProtectedRoute component to require specific system roles; add logic for admin role redirection

--- frontend/src/components/ProtectedRoute/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,13 +1,15 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContext";
+import { SystemRole } from "../../types";
 
 interface ProtectedRouteProps {
   redirectTo: string;
   requireAuth?: boolean; // If true, requires login. If false, only works for unauthenticated users.
+  requiredSystemRole?: SystemRole; // If true, requires admin role.
 }
 
-const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ redirectTo, requireAuth = true }) => {
-  const { user } = useAuth();
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ redirectTo, requireAuth = true, requiredSystemRole = "user" }) => {
+  const { user,  systemRole } = useAuth();
 
   if (requireAuth && !user) {
     return <Navigate to={redirectTo} />; // Redirect if login is required but the user is not authenticated
@@ -15,6 +17,10 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ redirectTo, requireAuth
 
   if (!requireAuth && user) {
     return <Navigate to="/" />; // Redirect if the user is logged in but trying to access a guest-only page
+  }
+
+  if (systemRole !== requiredSystemRole) {
+    return <Navigate to="/" />; // Redirect if the user is not an admin
   }
 
   return <Outlet />;
