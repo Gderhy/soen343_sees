3c469440964c04cf21a160068a6a0554a2a4e5d2
Gderhy <gabrielderhy@gmail.com>
2025-03-24 18:08:30 -0400

    Fix formatting in CreateEvent component by adding a space after participation variable

--- frontend/src/pages/CreateEvent/CreateEvent.tsx
+++ frontend/src/pages/CreateEvent/CreateEvent.tsx
@@ -75,7 +75,7 @@ const CreateEvent: React.FC = () => {
       eventDatetime,
       location,
       basePrice,
-      participation,
+      participation, 
       stakeholderIds
     );
     if (error) {
