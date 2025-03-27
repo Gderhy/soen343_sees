65eae69e35c742c145a7cbb5163b3342f9ae1e30
Baz <gab.fat8@gmail.com>
2025-03-27 02:06:42 -0400

    feat: setup of live events page for viewing and sending chats

---
+++ frontend/src/pages/LiveEvent/LiveEvent.tsx
@@ -0,0 +1,19 @@
+/**
+ * Live Events page where live chat can be found
+ */
+
+
+interface LiveEventProps{
+
+}
+
+export default function LiveEvent(props: LiveEventProps){
+
+
+    return(
+        <>
+            TEST Live Event Page        
+        </>
+    );
+
+}
